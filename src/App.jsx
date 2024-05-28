92672bc1b9ce075a092c002db519da40c3108c09
Zuhriddin1 <anarimon1985@gmail.com>
2024-05-28 15:15:50 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import List from "./components/List";
